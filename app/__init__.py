72c17003b0fefa1f2e00a6c90c0661d39295e99f
Thorn Brooks <95bthorn@gmail.com>
2026-03-17 11:15:11 -0500

    Migration Complete

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from flask_migrate import Migrate
 # import flask migrate here
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 # Instantiate Flask-Migrate library here
-
+migrate = Migrate(app,db)
 # Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
