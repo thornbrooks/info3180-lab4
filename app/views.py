522f655607b0be654a8a670940aeeaf1b82f0ffb
Thorn Brooks <95bthorn@gmail.com>
2026-03-17 14:27:56 -0500

    Edits were made to the views.py file

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -45,15 +46,28 @@ def login():
     # and not just one field
     if form.username.data:
         # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
 
+        user = UserProfile.query.filter_by(username=username).first()
+
+        
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
+        if user is not None and check_password_hash(user.password, password):
+                    # Gets user id, load into session
+                    login_user(user)
 
+                    flash('You have successfully logged in.', 'success')
+                    return redirect(url_for("upload"))
+        else:
+                    flash('Invalid username or password.', 'danger')
+
+       
         # Gets user id, load into session
-        login_user(user)
 
         # Remember to flash a message to the user
         return redirect(url_for("home"))  # The user should be redirected to the upload form instead
